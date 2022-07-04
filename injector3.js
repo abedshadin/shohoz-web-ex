3ca279b4bac01a1cf6f805878f3b6a07b2cb232c
abedshadin <abedshadin@gmail.com>
2022-07-04 09:26:47 +0600

    some update

--- injector3.js
+++ injector3.js
@@ -1,26 +1,54 @@
 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 selectAll();
 
-function selectAll(){
+function selectAll() {
 
-  const link =document.getElementsByClassName('seatsLayout');
-  link[3].click();
-  
-  
+  const link = document.getElementsByClassName('seatsLayout');
+  link[2].click();
+
+
   const myTimeout = setTimeout(select, 1000);
-  
+
   function select() {
-  
-  const link1 =document.getElementsByClassName('seat');
-  for(i=0; i<=500; i++){
+    const link1 = document.getElementsByClassName('seat');
+
+
+    for (i = 0; i <= 500; i++) {
       link1[i].click();
-  
- 
-  }
 
 
-  
-    
+    }
+
+
+
+
   }
-  
-  }
+
+}
--- selectSeats.js
+++ selectSeats.js
@@ -1,8 +1,8 @@
 select();
 function select() {
   
-    const link1 =document.getElementsByClassName('seat');
-    for(i=0; i<=800; i++){
+    const link1 = document.getElementsByClassName('seat');
+    for(i=0; i<=1000; i++){
         link1[i].click();
     
    
