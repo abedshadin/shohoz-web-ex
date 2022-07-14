d722d38db1a4fad9a75a5098241082b2e3b66fbe
abedshadin <abedshadin@gmail.com>
2022-07-14 19:49:32 +0600

    seat available update"

--- selectSeats.js
+++ selectSeats.js
@@ -1,8 +1,8 @@
 select();
 function select() {
   
-    const link1 = document.getElementsByClassName('seat');
-    for(i=0; i<=1000; i++){
+    const link1 = document.getElementsByClassName('seat-available');
+    for(i=0; i<=55; i++){
         link1[i].click();
     
    
